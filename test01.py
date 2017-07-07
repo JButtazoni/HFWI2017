7cad0ace18a4a6ea2cfe56558337a2ac48b42068
mugazombi <jbuttazoni@gmail.com>
2017-07-07 17:19:29 +0200

    Update and rename test01 to test01.py

---
+++ test01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+Name = raw_input("Wie heißen Sie? ")
+Alter = int(raw_input("Wie alt sind Sie? "))
+print "Sie heißen %s und sind %d Jahre alt." % (Name, Alter)
